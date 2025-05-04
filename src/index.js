d50c820129f6bae606a9421cb81e2c2d19e6d32f
4lokym <mykola.koshilko@gmail.com>
2025-05-04 16:38:47 +0200

    Add images switching buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import { dropDownMenu } from "4lokym-dropdownmenu";
 import "./style.css";
 
+document
+  .querySelector("nav")
+  .appendChild(
+    dropDownMenu.makeMenuButton(
+      new dropDownMenu.Option("testMenu", null, null, "white", "100%"),
+      [
+        new dropDownMenu.Option("test", () => alert("test")),
+        new dropDownMenu.Option("test", () => alert("test")),
+      ],
+      "6rem",
+      undefined,
+    ),
+  );
 
 document
   .querySelector("nav")
@@ -16,42 +29,49 @@ document
     ),
   );
 
-  document
-  .querySelector("nav")
-  .appendChild(
-    dropDownMenu.makeMenuButton(
-      new dropDownMenu.Option("testMenu", null, null, "white", "100%"),
-      [
-        new dropDownMenu.Option("test", () => alert("test")),
-        new dropDownMenu.Option("test", () => alert("test")),
-      ],
-      "6rem",
-      undefined,
-    ),
-  );
-
-
-const transitioning = function objectThatManagesTheTransitioning(){
+const transitioning = (function objectThatManagesTheTransitioning() {
   let count = 1;
-  const images = [...document.querySelectorAll(".img")]
+  const images = [...document.querySelectorAll(".img")];
   const num = images.length;
 
-  function print(){
+  const btn_container = document.querySelector(".btn-container");
+
+  function print() {
     console.log(images);
   }
 
-  const change = function(){
+  const change = function makeAllImagesTransition_OnLastGoBackToStart() {
     console.log("transition");
-    if(count >= num){
+    if (count >= num) {
       count = 0;
     }
-    images.map((x) =>{
-      x.style.transform = `translateX(${-count *100}%)`;
-    })
+    images.map((x) => {
+      x.style.transform = `translateX(${-count * 100}%)`;
+    });
     count++;
-  }
+  };
 
-  return {change, print}
-}();
+  const addButton = function addASingleButton(temp) {
+    const button = document.createElement("button");
+    button.addEventListener("click", () => {
+      images.map((x) => {
+        x.style.transform = `translateX(${-temp * 100}%)`;
+      });
+      count = temp+1;
+    });
+    btn_container.appendChild(button);
+  };
 
+  const addButtons = function addAsManyButtonsAsTheImages() {
+    let temp = 0;
+
+    for (temp; temp < num; temp++) {
+      addButton(temp);
+    }
+  };
+
+  return { change, print, addButtons };
+})();
+
+transitioning.addButtons();
 setInterval(transitioning.change, 10000);
